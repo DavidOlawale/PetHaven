bbf30994aa8efbda1971cc23df2da8ba69e6a5b5
Olawale David <olawaledavid11@gmail.com>
2025-03-14 00:58:46 -0700

    Initial commit

---
+++ PetHaven.Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Data/AppDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using PetHaven.Data.Model;
+
+namespace PetHaven.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    
+}
---
+++ PetHaven.Data/Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHaven.Data.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
